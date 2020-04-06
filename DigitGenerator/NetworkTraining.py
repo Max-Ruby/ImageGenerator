2f2f47de6dba8e4bc2953cdd1719400d86581911
laptop <mruby@protonmail.com>
2020-04-06 18:18:41 -0400

    Updated metrics, trimmed parameters.
    Network appears to train satisfactorily.
    
    Documentation will be next update.

--- DigitGenerator/NetworkTraining.py
+++ DigitGenerator/NetworkTraining.py
@@ -21,17 +21,37 @@ Various Notes:
 def grab_discriminator_samples(dataset, labels, samples, index, permutation, model):
     train_mixed_digits = np.zeros([samples,28,28,1])
     train_mixed_labels = np.zeros([samples,11])
-    for j in range(0, (10*samples) // 11):
+    for j in range(0, samples):
+        train_mixed_digits[j, :, :, :] = dataset[permutation[(index*samples) + j], :, :, :]
+        for k in range(0,10):
+            train_mixed_labels[j,k] = labels[permutation[(index*samples)+j],k]
+    return train_mixed_digits, train_mixed_labels
+
+
+def grab_discriminator_real_samples(dataset, labels, samples, index, permutation):
+    train_mixed_digits = np.zeros([samples,28,28,1])
+    train_mixed_labels = np.zeros([samples,11])
+    for j in range(0, samples// 2):
         train_mixed_digits[j, :, :, :] = dataset[permutation[(index*samples) + j], :, :, :]
         for k in range(0,10):
             train_mixed_labels[j,k] = labels[permutation[(index*samples)+j],k]
     fakes = grab_generated_samples(samples//2, model)
-    for j in range((10*samples)//11, samples):
+    for j in range(samples//2, samples):
         train_mixed_digits[j, :, :, :] = fakes[j - samples//2, :, :, :]
         train_mixed_labels[j,:] = np.zeros([11])
         train_mixed_labels[j,10] = 1
     return train_mixed_digits, train_mixed_labels
 
+
+def grab_discriminator_fake_samples(samples, model):
+    train_mixed_digits = np.zeros([samples,28,28,1])
+    train_mixed_labels = np.zeros([samples,11])
+    fakes = grab_generated_samples(samples//2, model)
+    for j in range(0, samples):
+        train_mixed_digits[j, :, :, :] = fakes[j - samples//2, :, :, :]
+        train_mixed_labels[j,:] = np.zeros([11])
+        train_mixed_labels[j,10] = 1
+    return train_mixed_digits, train_mixed_labels
 
 def embedding(val):
     emb = np.zeros([10])
@@ -48,15 +68,15 @@ def grab_generated_samples(samples, model):
     return synth_images
 
 # Load Models
-latent_dimension = 128
+latent_dimension = 256
 model = digit_generator(latent_dimension,10)
 discriminator = digit_discriminator(10)
 
 # Set Training Parameters
 batch_size = 32
 epochs = 100
-learnrate = 1e-4
-learnrate_disc = 1e-5
+learnrate = 5e-4
+learnrate_disc = 5e-5
 label_noise_level = 0.00
 
 # Load Data
@@ -100,9 +120,12 @@ for epoch in range(epochs):
         start = time.time()
         print("Epoch " + str(epoch + 1) + "/" + str(epochs) + ": Batch " +str(i+1) +"/" +str(num_batches))
         # Grab Real samples and Synthetic samples, for discriminator training
-        train_mixed_digits, train_mixed_labels = grab_discriminator_samples(train_images,train_labels,batch_size,i,permutation, model)
-
-        disc_mets = discriminator.train_on_batch(train_mixed_digits, train_mixed_labels)
+#        train_mixed_digits, train_mixed_labels = grab_discriminator_samples(train_images,train_labels,batch_size,i,permutation, model)
+        train_fake_digits, train_fake_labels = grab_discriminator_fake_samples(batch_size//2,model)
+        train_real_digits, train_real_labels = grab_discriminator_real_samples(train_images,train_labels,batch_size//2,i,permutation)
+        #disc_mets = discriminator.train_on_batch(train_mixed_digits, train_mixed_labels)
+        disc_mets_1 = discriminator.train_on_batch(train_fake_digits,train_fake_labels)
+        disc_mets_2 = discriminator.train_on_batch(train_real_digits,train_real_labels)
         train_noise = 2 * np.random.random([batch_size, latent_dimension]) - 1
         digits = np.zeros([batch_size, 10])
         target_fool = np.zeros([batch_size,11])
@@ -112,8 +135,11 @@ for epoch in range(epochs):
                 target_fool[j,k] = digits[j,k]
         model_mets = gan.train_on_batch([train_noise,digits], target_fool)
         time_taken = time.time() - start
-        print("Discriminator accuracy:" +str(disc_mets[1]) + "\t Generator Fool Rate:" + str(model_mets[1]) + "\t Estimated time per epoch:" + str(int(time_taken * num_batches)) + " seconds")
+        print("Discriminator accuracy:" +str(disc_mets_1[1]) + "\t Generator Fool Rate:" + str(model_mets[1]) + "\t Estimated time per epoch:" + str(int(time_taken * num_batches)) + " seconds")
 
+        print("Discriminator loss:" + '%.5f' % disc_mets_1[0] + "\t Generator loss:" + str(model_mets[0]))
+
+        print("Discriminator classification accuracy:" + str(disc_mets_2[1]) + "\t Discriminator classification loss:" + str(disc_mets_2[0]))
     ## Generate images for the given epoch
     for i in range(10):
         noise = 2 * np.random.random([10, latent_dimension]) - 1
